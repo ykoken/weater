fdeae1b3a07e3fb948c1f5f34aa2e84570e34871
ykoken <ykoken@msn.com>
2020-11-14 23:39:02 +0300

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('api_token', 80)->after('password')
+                ->unique()
+                ->nullable()
+                ->default(null);
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('mobile_number')->unique();
+            $table->text('profile_url');
+            $table->integer('city')->default(0);
+            $table->string('timezone');
+            $table->string('language');
+            $table->string('device_system');
+            $table->boolean('notification')->default(true);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
